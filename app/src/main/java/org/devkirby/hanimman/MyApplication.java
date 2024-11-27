28a4cc80efbb859651e032a761789624121b59c4
samgak <183370871+samgak0@users.noreply.github.com>
2024-11-27 11:21:52 +0900

    Default Notification Channel 추가

---
+++ app/src/main/java/org/devkirby/hanimman/MyApplication.java
@@ -0,0 +1,29 @@
+package org.devkirby.hanimman;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MyApplication extends Application {
+    public static final String CHANNEL_ID = "default_channel_id";
+    public static final String CHANNEL_NAME = "Default Channel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel.setDescription("This is the default notification channel.");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+}
