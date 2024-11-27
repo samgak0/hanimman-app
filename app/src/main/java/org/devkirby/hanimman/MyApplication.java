b9b1d2ba361b186b3230b07a8b47db31e7011e7e
samgak <183370871+samgak0@users.noreply.github.com>
2024-11-27 18:08:00 +0900

    알림 방시 변경

--- app/src/main/java/org/devkirby/hanimman/MyApplication.java
+++ app/src/main/java/org/devkirby/hanimman/MyApplication.java
@@ -1,13 +1,23 @@
 package org.devkirby.hanimman;
 
+import android.app.Activity;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class MyApplication extends Application {
     public static final String CHANNEL_ID = "default_channel_id";
     public static final String CHANNEL_NAME = "Default Channel";
+    private static boolean isAppInForeground = false;
+
+    public static boolean isAppInForeground() {
+        return isAppInForeground;
+    }
 
     @Override
     public void onCreate() {
@@ -25,5 +35,48 @@ public class MyApplication extends Application {
                 notificationManager.createNotificationChannel(channel);
             }
         }
+
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            private int activityReferences = 0;
+            private boolean isActivityChangingConfigurations = false;
+
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+                if (++activityReferences == 1 && !isActivityChangingConfigurations) {
+                    isAppInForeground = true;
+                }
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+                isActivityChangingConfigurations = activity.isChangingConfigurations();
+                if (--activityReferences == 0 && !isActivityChangingConfigurations) {
+                    isAppInForeground = false;
+                }
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+            }
+        });
     }
+
+
 }
--- app/src/main/java/org/devkirby/hanimman/MyFirebaseMessagingService.java
+++ app/src/main/java/org/devkirby/hanimman/MyFirebaseMessagingService.java
@@ -1,10 +1,6 @@
 package org.devkirby.hanimman;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -12,6 +8,9 @@ import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+
+import java.util.Map;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -19,44 +18,41 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-        Log.d("MyFirebaseMessagingService","onNewToken token = " + token);
+        Log.d("MyFirebaseMessagingService", "onNewToken token = " + token);
         sendRegistrationToServer(token);
     }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.d("MyFirebaseMessagingService","remoteMessage.getTitle = "+remoteMessage.getNotification().getTitle());
-        Log.d("MyFirebaseMessagingService","remoteMessage.getBody = "+remoteMessage.getNotification().getBody());
-        if (remoteMessage.getNotification() != null) {
-            showNotification(remoteMessage.getNotification().getTitle(),
-                    remoteMessage.getNotification().getBody());
+        if (MyApplication.isAppInForeground()) {
+            Log.d("FirebaseMessaging", "App is in foreground");
+        } else {
+            Log.d("FirebaseMessaging", "App is in background");
         }
+        Map<String, String> data = remoteMessage.getData();
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(remoteMessage.getData());
+        Log.d("MyFirebaseMessagingService", "remoteMessage.getData() = " + jsonString);
+        showNotification(data.get("title"),
+                data.get("content"));
     }
+
     private void showNotification(String title, String message) {
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        String channelId = "fcm_default_channel";
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId, "FCM Notifications",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            manager.createNotificationChannel(channel);
-        }
-
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        String channelId = MyApplication.CHANNEL_ID;
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         manager.notify(0, builder.build());
     }
 
     private void sendRegistrationToServer(String token) {
-        Log.d("MyFirebaseMessagingService","token = "+ token);
+        Log.d("MyFirebaseMessagingService", "token = " + token);
     }
 }
